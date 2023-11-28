9dba1688de262925c4c01b3cb63cbca0a9a1ce00
JocmanHUN <135754827+JocmanHUN@users.noreply.github.com>
2023-11-28 18:37:37 +0100

    Apró módosítás a views.py-ban

--- App/AFP1_Nagy_Project/main/register/views.py
+++ App/AFP1_Nagy_Project/main/register/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .forms import SignUpForm
 
+
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/login')
+            return redirect("/login")
     else:
         form = SignUpForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
