7dd2edf8296fb7bb591d0b79088d347e5b16c565
Drntth <tothdi03@gmail.com>
2023-12-07 10:26:35 +0100

    User oldal auth_user felhasználók

--- App/AFP1_Nagy_Project/main/users/views.py
+++ App/AFP1_Nagy_Project/main/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import User
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 @login_required(redirect_field_name="bejelentkezes_szukseges")
