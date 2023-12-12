1cb7981e20d53d7080f499e72c7fb65329bc35e7
MizereRobin <mizererobin@gmail.com>
2023-12-12 03:08:01 +0100

    ChatGPT - Working

--- App/AFP1_Nagy_Project/main/GPTapi/views.py
+++ App/AFP1_Nagy_Project/main/GPTapi/views.py
@@ -1,35 +1,26 @@
 from django.shortcuts import render
+import openai
+from django.shortcuts import render
+from openai import chat
 
-import requests
-import OpenAI
-from django.http import JsonResponse
-
-OpenAI.api_key="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"
-
-def index(request):
-   return render(request, 'gpt.html')
-
-def api_request_view(request):
-    url = 'https://chatgpt-api.shn.hk/v1/'  # Replace with your API endpoint
-    
-    # Make a GET request to the API
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        # Process the API response data as needed
-        return JsonResponse(data)
+api_key="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"
+URL = "https://chatgpt-api.shn.hk/v1/"
+default =""
+def chatbot(request):
+    chatbot_response = None
+    if request.method == 'POST':
+        #URL = "https://chatgpt-api.shn.hk/v1/ \-H 'Content-Type: application/json' \-d '{   model": "gpt-3.5-turbo",   "messages": [{"role": "user", "content": "Hello, how are you?"}]}'"
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+        response = chat.completions.create(
+        messages = [{"role": "user", "content": user_input}],
+        model="gpt-3.5-turbo-1106",
+        max_tokens = 256,
+        temperature=0.5,
+        )
+        
+        chatbot_response=str(response.choices[0].message.content)
+        return render(request, "gpt.html",{"response":chatbot_response})
     else:
-        return JsonResponse({'error': 'Failed to fetch data'}, status=500)
-
-
-client = OpenAI()
-
-stream = client.chat.completions.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": "Say this is a test"}],
-    stream=True,
-)
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+        return render(request, "gpt.html",{"response":default})
